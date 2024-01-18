7919c8820c372f65de45971589604db161963eab
Aliaksei Drabushau <a.drabushev@geozilla.com>
2024-01-18 16:42:30 +0000

    basic

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+// const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'index.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/, // Include .jsx files
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      /*{
+        test: /\.(png|jpg|jpeg|gif|webp|mp4)/,
+        use: ['file-loader']
+      },*/
+    ],
+  },
+  externals: {
+    react: 'react',
+    'react-dom': 'react-dom',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  /*optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          format: {
+            comments: false,
+          },
+        },
+        extractComments: false,
+      }),
+    ],
+  },*/
+};
