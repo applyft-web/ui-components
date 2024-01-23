9e3beb6d34360583622aac00d1516a513bb89820
Aliaksei Drabushau <a.drabushev@geozilla.com>
2024-01-23 18:05:27 +0000

    deps update

--- webpack.config.js
+++ webpack.config.js
@@ -7,9 +7,6 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: '[name].js',
-    publicPath: '',
-    chunkFilename: '[name].js',
-    sourceMapFilename: '[name].js.map',
     library: '[name]'
   },
   module: {
