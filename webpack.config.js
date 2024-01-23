1b88c19ea69439ce0104e9ff756025058891f8a9
Aliaksei Drabushau <a.drabushev@geozilla.com>
2024-01-23 18:13:33 +0000

    deps update

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: path.resolve(__dirname, './src/index.js'),
   output: {
     path: path.resolve(__dirname, 'build'),
-    filename: '[name].js',
+    filename: 'index.js',
     library: '[name]'
   },
   module: {
