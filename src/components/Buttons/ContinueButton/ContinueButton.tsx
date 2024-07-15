f9da770272359a7dfb39717927d66427e0fb2388
Aliaksei Drabushau <a.drabushev@geozilla.com>
2024-07-15 22:54:18 +0100

    continue button update

--- src/components/Buttons/ContinueButton/ContinueButton.tsx
+++ src/components/Buttons/ContinueButton/ContinueButton.tsx
@@ -1,79 +1,7 @@
 import React from 'react';
-import styled from 'styled-components';
 import { useTranslation } from 'react-i18next';
-import { getCssSize, getFormattedStyles } from '../../../utils';
-
-export interface ButtonCustomStylesWithStatesProps {
-  readonly default?: string;
-  readonly disabled?: string;
-  readonly hover?: string;
-}
-
-interface StyledButtonProps {
-  readonly $customStyles?: ButtonCustomStylesWithStatesProps;
-  readonly $mt?: number | string;
-  readonly $mb?: number | string;
-  readonly $staticPosition?: boolean;
-}
-
-const StyledButton = styled.button<StyledButtonProps>`
-  display: block;
-  width: 100%;
-  max-width: ${({ theme }) => theme?.maxContentWidth}px;
-  height: 56px;
-  min-height: 30px;
-  background-color: ${({ theme }) => theme?.colors?.primary};
-  border-radius: ${({ theme }) => theme?.buttonBorderRadius};
-  border: none;
-  padding: 0 20px;
-  font-weight: 500;
-  font-size: 16px;
-  line-height: 1;
-  color: ${({ theme }) => theme?.colors?.bodyBackground};
-  overflow: hidden;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-  position: fixed;
-  bottom: ${({ theme }) => theme?.buttonBottomPosition};
-  left: 50%;
-  transform: translateX(-50%);
-  transition: .3s;
-  cursor: pointer;
-  margin-left: auto;
-  margin-right: auto;
-  ${({ $mt }) => $mt && `margin-top: ${getCssSize($mt)}`};
-  ${({ $mb }) => $mb && `margin-bottom: ${getCssSize($mb)}`};
-  ${({ $staticPosition }) => $staticPosition && `
-    position: relative;
-    bottom: auto;
-    left: auto;
-    transform: none;
-  `};
-
-  &:disabled {
-    ${({ theme }) => `
-      background-color: ${theme?.colors?.buttonDisabled};
-      color: ${theme?.colors?.buttonDisabledTextColor};
-      pointer-events: none;
-    `};
-
-    ${({ $customStyles }) => $customStyles?.disabled};
-  }
-
-  &:focus {
-    outline: none;
-  }
-
-  @media screen and (min-width: ${({ theme }) => theme?.desktopMinWidth}px) and (hover: hover) {
-    &:hover {
-      background-color: ${({ theme }) => theme?.colors?.buttonHover};
-
-      ${({ $customStyles }) => $customStyles?.hover};
-    }
-  }
-
-  ${({ $customStyles }) => $customStyles?.default};
-`;
+import { getFormattedStyles } from '../../../utils';
+import * as S from './styled';
 
 export interface ContinueButtonProps {
   children?: React.ReactNode | string;
@@ -82,7 +10,7 @@ export interface ContinueButtonProps {
   mt?: string | number;
   mb?: string | number;
   staticPosition?: boolean;
-  customStyles?: ButtonCustomStylesWithStatesProps | string;
+  customStyles?: S.ButtonCustomStylesWithStatesProps | string;
   theme?: object;
   [propName: string]: any;
 }
@@ -98,10 +26,10 @@ export const ContinueButton = ({
   ...rest
 }: ContinueButtonProps) => {
   const { t } = useTranslation();
-  const styles: ButtonCustomStylesWithStatesProps = getFormattedStyles(customStyles, 'default');
+  const styles: S.ButtonCustomStylesWithStatesProps = getFormattedStyles(customStyles, 'default');
 
   return (
-    <StyledButton
+    <S.StyledButton
       onClick={onClick}
       id={customId}
       $mt={mt}
@@ -111,6 +39,6 @@ export const ContinueButton = ({
       {...rest}
     >
       {children ?? t('Continue')}
-    </StyledButton>
+    </S.StyledButton>
   );
 };
--- src/components/Buttons/ContinueButton/index.ts
+++ src/components/Buttons/ContinueButton/index.ts
@@ -1,5 +1,5 @@
 export {
   ContinueButton,
   type ContinueButtonProps,
-  type ButtonCustomStylesWithStatesProps,
 } from './ContinueButton';
+export { type ButtonCustomStylesWithStatesProps } from './styled';
